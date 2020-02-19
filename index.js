4f2aeb82df88d5c581806180ca207c0108a017dc
Christian_Polanco <chrispolanco@sbcglobal.net>
2020-02-18 22:55:53 -0800

    Done.

--- index.js
+++ index.js
@@ -1,2 +1,20 @@
 
-// Write your code here
+//1
+
+let num1 = 2
+let num2 = 31
+let multiply = num1 * num2; 
+
+//2
+
+let random = Math.random(); 
+
+//3 
+
+let mod = 16 % 6; 
+
+//4 
+
+let max = Math.max(1, 2, 3, 19, 20); 
+
+
